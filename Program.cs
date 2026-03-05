b825db68555870479f19efb681024b4964c4fad1
Rennan Carneiro <rennanbarros10@gmail.com>
2026-03-05 15:53:09 -0300

    feat: integrate Swagger for API documentation and add OpenAPI support

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddOpenApi();
 //injeção de dependencia
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=assetflow.db")); //configuração do sqlite
 builder.Services.AddControllers(); //para procurar na pasta controllers
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.MapControllers();
@@ -17,33 +19,13 @@ app.MapControllers();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty; // Isso faz o Swagger abrir direto no localhost:5037/
+    });
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
